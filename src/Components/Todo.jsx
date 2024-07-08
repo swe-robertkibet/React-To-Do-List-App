a482d521281add60282cb125443c94de49c8f531
swe-robertkibet <swe.robertkibet@gmail.com.com>
2024-07-09 01:30:36 +0300

    Add functions for handling text inputs

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,15 +1,43 @@
+import { useEffect } from "react";
 import "./CSS/Todo.css";
+import { useState, useRef } from "react";
 
+let count = 0;
 const Todo = () => {
   //functions
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
 
+  const add = () => {
+    setTodos([
+      ...todos,
+      { no: count++, text: inputRef.current.value, display: "" },
+    ]);
+    inputRef.current.value = "";
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
   //return statement
   return (
     <div className="todo">
       <div className="todo-header">To-Do List</div>
       <div className="todo-add">
-        <input type="text" placeholder="Add Your Task" className="todo-input" />
-        <div className="todo-add-btn">ADD</div>
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Add Your Task"
+          className="todo-input"
+        />
+        <div
+          onClick={() => {
+            add();
+          }}
+          className="todo-add-btn"
+        >
+          ADD
+        </div>
       </div>
 
       <div className="todo-list"></div>
