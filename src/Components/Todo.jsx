0b03889c59147c48d988e87ea1cbc5a60f9c0a15
swe-robertkibet <swe.robertkibet@gmail.com.com>
2024-07-09 01:11:15 +0300

    Add styles to input, button, header and the container

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -4,7 +4,17 @@ const Todo = () => {
   //functions
 
   //return statement
-  return <div></div>;
+  return (
+    <div className="todo">
+      <div className="todo-header">To-Do List</div>
+      <div className="todo-add">
+        <input type="text" placeholder="Add Your Task" className="todo-input" />
+        <div className="todo-add-btn">ADD</div>
+      </div>
+
+      <div className="todo-list"></div>
+    </div>
+  );
 };
 
 export default Todo;
